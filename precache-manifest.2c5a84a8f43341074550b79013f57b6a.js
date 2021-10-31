e56a74f3523c314261aa65239d77fa150c3968c9
leejaek <leejaek@users.noreply.github.com>
2021-10-31 08:05:02 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.2c5a84a8f43341074550b79013f57b6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d37510cf525bb1301074d0bb666bfd8",
+    "url": "https://leejaek.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "6c9f0a5a18c8708c03c0",
+    "url": "https://leejaek.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "2f307dc11544d999e994",
+    "url": "https://leejaek.github.io/task-management-frontend/static/js/2.822f35ac.chunk.js"
+  },
+  {
+    "revision": "6c9f0a5a18c8708c03c0",
+    "url": "https://leejaek.github.io/task-management-frontend/static/js/main.328d1c44.chunk.js"
+  },
+  {
+    "revision": "c104d787ba775000b441",
+    "url": "https://leejaek.github.io/task-management-frontend/static/js/runtime~main.93e47efc.js"
+  }
+]);
